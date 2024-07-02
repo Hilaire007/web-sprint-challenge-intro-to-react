c6e46a99faaa44f92ee09e0b4220ddda45f0e998
Nicholas Hilaire <131618569+Hilaire007@users.noreply.github.com>
2024-07-02 19:05:18 -0400

    Add files via upload

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./Character";
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = "http://localhost:9009/api/planets";
+const urlPeople = "http://localhost:9009/api/people";
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [data, setData] = useState([]);
+
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    async function getData() {
+      const [resPlanets, resPeople] = await Promise.all([
+        axios.get(urlPlanets),
+        axios.get(urlPeople),
+      ]);
+      let character = resPeople.data.map((char) => {
+        return {
+          ...char,
+          homeworld: resPlanets.data.find(
+            (world) => world.id == char.homeworld
+          ),
+        };
+      });
+      setData(character);
+    }
+    getData();
+  }, []);
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      <p>
+        See the README of the project for instructions on completing this
+        challenge
+      </p>
+      {data.map((character) => (
+        <Character key={character.id} data={character} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = App
+if (typeof module !== "undefined" && module.exports) module.exports = App;
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Character() { // ❗ Add the props
+function Character({ data }) {
+  // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [homeWorld, setHomeWorld] = useState(false);
+  const toggle = () => {
+    setHomeWorld(!homeWorld);
+  };
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card" onClick={toggle}>
+      <h3 className="character-name">{data.name}</h3>
+      {homeWorld && (
+        <p>
+          Planet:{" "}
+          <span className="character-planet">{data.homeworld.name}</span>
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Character
+export default Character;
